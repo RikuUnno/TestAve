ead98b0afff14db41c9a0f506bee319a3c7933e1
RikuUnno <riku.unno@gmail.com>
2024-10-21 22:50:12 +0900

    プロジェクト ファイルを追加します。

---
+++ TestAve.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numberOfPeople;
+	double ave;
+	double total = 0;
+
+	cout << "何名入力しますか？" << endl;
+	cin >> numberOfPeople;
+
+	cout << endl;
+
+	int* p1 = new int[numberOfPeople];
+
+	for (int i = 0; i < numberOfPeople; i++)
+	{
+		cout << i + 1 << "人目を入力してください。" << flush;
+		cin >> p1[i];
+		total += p1[i];
+	}
+
+	ave = total / numberOfPeople;
+
+	cout << endl;
+
+	cout << "平均点は" << ave << "です。" << endl;
+}
